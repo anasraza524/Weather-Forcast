2df9a83b8978f20ab53317a8327d6fdfd2d682f4
Muhammad Anas Raza Siddiqui <85056487+anasraza524@users.noreply.github.com>
2022-08-31 17:16:32 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -41,22 +41,31 @@ console.log("sh",previousEachHour, currentEachHour)
                     let sumTemp = Number(previousEachHour.main.temp) + Number(currentEachHour.main.temp)
                     let sumMinTemp = Number(previousEachHour.main.temp_min) + Number(currentEachHour.main.temp_min)
                     let sumMaxTemp = Number(previousEachHour.main.temp_max) + Number(currentEachHour.main.temp_max)
+                   let sumsea_level =Number(previousEachHour.main.sea_level) + Number(currentEachHour.main.sea_level)
+                   let sumHumidity =Number(previousEachHour.main.humidity) + Number(currentEachHour.main.humidity)
                     return {
                         main: {
                             temp: sumTemp,
                             temp_min: sumMinTemp,
                             temp_max: sumMaxTemp,
+                            humidity: sumsea_level,
+                        sea_level: sumHumidity,
                            
                         },
                         dt_txt: currentEachHour.dt_txt,
                         length: eachDay.length,
-                        icon: currentEachHour.weather[0].description
+                        description: currentEachHour.weather[0].description,
+                        icon: currentEachHour.weather[0].icon,
+                        windSpeed: currentEachHour.wind.speed,
+                       
                     }
                 }, {
                     main: {
                         temp: 0,
                         temp_min: 0,
                         temp_max: 0,
+                        humidity:0,
+                        sea_level:0
                         
                     }
                 })
@@ -73,7 +82,7 @@ console.log("sh",previousEachHour, currentEachHour)
                     <div class="forcastCard">
         <div class="border">   
 <span class="day">${moment(eachDay.dt_txt).format('ddd')}</span>
-<p>${eachDay.icon}</p>
+
 <div class="Min">Temp Min: ${Math.floor(eachDay.main.temp_min / eachDay.length)}</div>
 <div class="Max">Temp Max: ${Math.floor(eachDay.main.temp_max / eachDay.length)}</div>
 </div>
